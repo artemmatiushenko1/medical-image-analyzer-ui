90422dac75c3c4f84bca065c0168d2d4621a9f0e
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-04-13 10:03:51 +0300

    add setting icon

--- src/pages/new-image/libs/components/image-crop-dialog/CropSettingsForm.tsx
+++ src/pages/new-image/libs/components/image-crop-dialog/CropSettingsForm.tsx
@@ -1,18 +1,26 @@
+import { CropRotateRounded, ZoomInRounded } from '@mui/icons-material';
 import { Box, Checkbox, Slider, Typography } from '@mui/material';
+import { styles } from './styles';
 
 const CropSettingsForm = () => {
   return (
     <>
       <Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography variant="caption">Rotate</Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Box sx={styles.settingTitleRoot}>
+            <CropRotateRounded sx={styles.settingIcon} />
+            <Typography variant="caption">Rotate</Typography>
+          </Box>
           <Typography variant="caption">90&deg;</Typography>
         </Box>
         <Slider />
       </Box>
       <Box>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Typography variant="caption">Scale</Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Box sx={styles.settingTitleRoot}>
+            <ZoomInRounded sx={styles.settingIcon} />
+            <Typography variant="caption">Scale</Typography>
+          </Box>
           <Typography variant="caption">90%</Typography>
         </Box>
         <Slider />
--- src/pages/new-image/libs/components/image-crop-dialog/styles.ts
+++ src/pages/new-image/libs/components/image-crop-dialog/styles.ts
@@ -30,12 +30,22 @@ const styles = createStyleSheet({
   settingsSectionRoot: {
     gap: 1,
   },
+  settingIcon: {
+    fontSize: '15px',
+    color: ({ palette }) => palette.text.secondary,
+  },
+  settingTitleRoot: {
+    display: 'flex',
+    gap: 1,
+    alignItems: 'center',
+  },
 });
 
 const StyledReactCrop = styled(ReactCrop)(({ theme: { shape } }) => ({
   alignSelf: 'self-start',
   borderRadius: shape.borderRadius * 3,
   overflow: 'hidden',
+  maxHeight: 600,
 }));
 
 export { styles, StyledReactCrop };
