4c49f7494604f38d8b303d90162844a66ce2913b
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-04-20 15:49:04 +0300

    add mui date picker package

--- src/libs/components/app/index.tsx
+++ src/libs/components/app/index.tsx
@@ -1,15 +1,10 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { routes } from './routes';
-import { ThemeConfig } from '@/libs/theme';
 
 const router = createBrowserRouter(routes);
 
 const App = () => {
-  return (
-    <ThemeConfig>
-      <RouterProvider router={router} />
-    </ThemeConfig>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export { App };
