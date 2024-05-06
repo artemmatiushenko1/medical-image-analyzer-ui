f94b1480e24a97207010be19a34f21ad12e85a1c
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-05-06 18:08:23 +0300

    add architecture select

--- src/pages/new-study/libs/components/choose-diagnostics/selected-diagnostic-accordion.tsx
+++ src/pages/new-study/libs/components/choose-diagnostics/selected-diagnostic-accordion.tsx
@@ -4,7 +4,11 @@ import {
   AccordionDetails,
   AccordionSummary,
   Box,
+  FormControl,
   IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
   Typography,
 } from '@mui/material';
 import { selectedDiagnosticAccordion as styles } from './styles';
@@ -28,36 +32,52 @@ const SelectedDiagnosticAccordion = (
     onDelete(id);
   };
 
+  const ARCHITECTURE_OPTIONS = [
+    { key: '1', title: 'Feedforward Neural Networks (FNN)', value: '1' },
+    { key: '2', title: 'Recurrent Neural Networks (RNN)', value: '2' },
+    { key: '3', title: 'Generative Adversarial Networks (GAN)', value: '3' },
+  ];
+
   return (
-    <Accordion defaultExpanded sx={styles.root}>
-      <AccordionSummary expandIcon={<ExpandMoreRounded />}>
-        <Box
-          sx={{
-            flex: 1,
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}
-        >
-          <Typography variant="body2" fontWeight={500}>
-            {title}
-          </Typography>
-          <IconButton
-            size="small"
-            className="delete-icon"
-            onClick={handleDeleteButtonClick}
+    <>
+      <Accordion defaultExpanded sx={styles.root}>
+        <AccordionSummary expandIcon={<ExpandMoreRounded />}>
+          <Box
+            sx={{
+              flex: 1,
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
           >
-            <DeleteOutline />
-          </IconButton>
-        </Box>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography variant="body2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </Typography>
-      </AccordionDetails>
-    </Accordion>
+            <Typography variant="body2" fontWeight={500}>
+              {title}
+            </Typography>
+            <IconButton
+              size="small"
+              className="delete-icon"
+              onClick={handleDeleteButtonClick}
+            >
+              <DeleteOutline />
+            </IconButton>
+          </Box>
+        </AccordionSummary>
+        <AccordionDetails>
+          <FormControl size="small" variant="outlined" fullWidth>
+            <InputLabel htmlFor="test-select-label">
+              Model Architecture
+            </InputLabel>
+            <Select id="test-select-label" label="Model Architecture">
+              {ARCHITECTURE_OPTIONS.map(({ key, value, title }) => (
+                <MenuItem key={key} value={value}>
+                  <Typography variant="body2">{title}</Typography>
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </AccordionDetails>
+      </Accordion>
+    </>
   );
 };
 
