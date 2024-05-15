c5bc43d1c2147ed85d201f57aa8a69c55fefedcc
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-05-15 10:41:50 +0300

    remove unused pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,3 @@
-export { Home } from './home/home.page';
-export { Viewer } from './viewer/viewer.page';
 export { NewStudy } from './new-study/new-study.page';
 export { SignIn } from './auth';
 export { Studies } from './studies';
--- src/pages/viewer/viewer.page.tsx
+++
@@ -1,5 +0,0 @@
-const Viewer = () => {
-  return <div>Viewer</div>;
-};
-
-export { Viewer };
