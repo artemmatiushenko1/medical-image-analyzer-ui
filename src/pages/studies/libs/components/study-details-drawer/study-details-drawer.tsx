8fa35233c9f9f26ff8fbf9d39879f89434f9b611
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-05-15 13:39:53 +0300

    handle study status at details drawer

--- src/pages/studies/libs/components/study-details-drawer/study-details-drawer.tsx
+++ src/pages/studies/libs/components/study-details-drawer/study-details-drawer.tsx
@@ -1,4 +1,4 @@
-import { Study } from '@/packages/studies';
+import { Study, StudyStatus } from '@/packages/studies';
 import { CloseRounded, EventNote } from '@mui/icons-material';
 import {
   Box,
@@ -63,7 +63,7 @@ const StudyDetailsDrawer = (props: StudyDetailsDrawerProps) => {
         </Box>
       </DialogTitle>
       <DialogContent>
-        <Stack gap={2}>
+        <Stack gap={3}>
           <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
             <StudyStatusChip status={study.status} />
             <DetailItemText iconComponent={EventNote}>
@@ -87,7 +87,6 @@ const StudyDetailsDrawer = (props: StudyDetailsDrawerProps) => {
                 objectFit: 'contain',
               }}
             >
-              {/* Create a separate component to display an image -> it's already duplicated accross multiple places */}
               <Box
                 component="img"
                 sx={{
@@ -100,7 +99,7 @@ const StudyDetailsDrawer = (props: StudyDetailsDrawerProps) => {
               />
             </Box>
           </Stack>
-          <Stack gap={3}>
+          {study.status === StudyStatus.PENDING && (
             <Paper
               sx={{
                 p: 2,
@@ -115,126 +114,135 @@ const StudyDetailsDrawer = (props: StudyDetailsDrawerProps) => {
                 />
                 <Typography variant="caption">
                   There's no result available yet.
-                  <br /> The study is still being processed by AI.
+                  <br /> The image is still being examined by AI.
                 </Typography>
               </Stack>
             </Paper>
-            <Stack gap={2}>
-              <Typography
-                fontSize="14px"
-                fontWeight={600}
-                sx={{ color: ({ palette }) => palette.neutral.dark }}
-              >
-                Confidence
-              </Typography>
-              <Box
-                sx={{
-                  display: 'flex',
-                  gap: 2,
-                  alignItems: 'center',
-                }}
-              >
-                <Box sx={{ position: 'relative' }}>
-                  <CircularProgress
-                    variant="determinate"
-                    value={100}
-                    sx={{
-                      color: ({ palette }) => palette.error.light,
-                    }}
-                    size={CONFIDENCE_PROGRESS_SIZE}
-                    thickness={CONFIDENCE_PROGRESS_THICKNESS}
-                  />
-                  <CircularProgress
-                    variant="determinate"
-                    value={87}
-                    color="error"
-                    size={CONFIDENCE_PROGRESS_SIZE}
-                    thickness={CONFIDENCE_PROGRESS_THICKNESS}
-                    sx={{
-                      strokeLinecap: 'round',
-                      position: 'absolute',
-                      inset: 0,
-                    }}
-                  />
-                  <Box
-                    sx={{
-                      position: 'absolute',
-                      inset: 0,
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      height: CONFIDENCE_PROGRESS_SIZE,
-                    }}
-                  >
-                    <Typography
-                      variant="subtitle2"
-                      fontSize="16px"
+          )}
+          {study.status === StudyStatus.COMPLETED && (
+            <Stack gap={3}>
+              <Stack gap={2}>
+                <Typography
+                  fontSize="14px"
+                  fontWeight={600}
+                  sx={{ color: ({ palette }) => palette.neutral.dark }}
+                >
+                  Confidence
+                </Typography>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    gap: 2,
+                    alignItems: 'center',
+                  }}
+                >
+                  <Box sx={{ position: 'relative' }}>
+                    <CircularProgress
+                      variant="determinate"
+                      value={100}
                       sx={{
-                        display: 'inline-block',
+                        color: ({ palette }) => palette.error.light,
+                      }}
+                      size={CONFIDENCE_PROGRESS_SIZE}
+                      thickness={CONFIDENCE_PROGRESS_THICKNESS}
+                    />
+                    <CircularProgress
+                      variant="determinate"
+                      value={87}
+                      color="error"
+                      size={CONFIDENCE_PROGRESS_SIZE}
+                      thickness={CONFIDENCE_PROGRESS_THICKNESS}
+                      sx={{
+                        strokeLinecap: 'round',
+                        position: 'absolute',
+                        inset: 0,
+                      }}
+                    />
+                    <Box
+                      sx={{
+                        position: 'absolute',
+                        inset: 0,
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        height: CONFIDENCE_PROGRESS_SIZE,
                       }}
                     >
-                      87.5%
-                    </Typography>
+                      <Typography
+                        variant="subtitle2"
+                        fontSize="16px"
+                        sx={{
+                          display: 'inline-block',
+                        }}
+                      >
+                        87.5%
+                      </Typography>
+                    </Box>
                   </Box>
+                  <Stack>
+                    <Typography fontWeight={600}>+ Positive</Typography>
+                    <Typography variant="caption">
+                      There's a high risk of a disease been studied.
+                    </Typography>
+                  </Stack>
                 </Box>
-                <Stack>
-                  <Typography fontWeight={600}>+ Positive</Typography>
-                  <Typography variant="caption">
-                    There's a high risk of a disease been studied.
-                  </Typography>
-                </Stack>
-              </Box>
-            </Stack>
-            <Stack gap={2}>
-              <Typography
-                fontSize="14px"
-                fontWeight={600}
-                sx={{ color: ({ palette }) => palette.neutral.dark }}
-              >
-                Report
-              </Typography>
-              <Paper
-                sx={{
-                  px: 3,
-                  py: 1,
-                  pr: 2,
-                  backgroundColor: ({ palette }) => palette.primary.main,
-                  border: 'none',
-                  borderRadius: ({ shape }) => shape.borderRadius,
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  alignItems: 'center',
-                }}
-              >
+              </Stack>
+              <Stack gap={2}>
                 <Typography
-                  variant="subtitle2"
-                  sx={{ color: ({ palette }) => palette.primary.contrastText }}
+                  fontSize="14px"
+                  fontWeight={600}
+                  sx={{ color: ({ palette }) => palette.neutral.dark }}
                 >
-                  Класифікація_COVID-аномалій_{study.date?.replaceAll(' ', '_')}
-                  .pdf
+                  Report
                 </Typography>
-                <IconButton
-                  sx={{ color: ({ palette }) => palette.primary.contrastText }}
+                <Paper
+                  sx={{
+                    px: 3,
+                    py: 1,
+                    pr: 2,
+                    backgroundColor: ({ palette }) => palette.primary.main,
+                    border: 'none',
+                    borderRadius: ({ shape }) => shape.borderRadius,
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                  }}
                 >
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    height="20px"
-                    width="20px"
-                    xmlns="http://www.w3.org/2000/svg"
+                  <Typography
+                    variant="subtitle2"
+                    sx={{
+                      color: ({ palette }) => palette.primary.contrastText,
+                    }}
                   >
-                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-                    <polyline points="7 10 12 15 17 10"></polyline>
-                    <line x1="12" y1="15" x2="12" y2="3"></line>
-                  </svg>
-                </IconButton>
-              </Paper>
+                    Класифікація_COVID-аномалій_
+                    {study.date?.replaceAll(' ', '_')}
+                    .pdf
+                  </Typography>
+                  <IconButton
+                    sx={{
+                      color: ({ palette }) => palette.primary.contrastText,
+                    }}
+                  >
+                    <svg
+                      stroke="currentColor"
+                      fill="none"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      height="20px"
+                      width="20px"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                      <polyline points="7 10 12 15 17 10"></polyline>
+                      <line x1="12" y1="15" x2="12" y2="3"></line>
+                    </svg>
+                  </IconButton>
+                </Paper>
+              </Stack>
             </Stack>
-          </Stack>
+          )}
         </Stack>
       </DialogContent>
     </Drawer>
