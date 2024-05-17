e166afdb448e41e509887e9723fd4df92432c7a6
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-05-17 22:23:51 +0300

    update dependency cruiser config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import { App } from './app';
 import '@fontsource/inter';
 import 'react-toastify/dist/ReactToastify.css';
