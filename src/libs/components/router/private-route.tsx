f4f55f5ea2e5246f97e9ae69aaf300ded1f465bc
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-05-19 21:47:15 +0300

    add roles prop

--- src/libs/components/router/private-route.tsx
+++ src/libs/components/router/private-route.tsx
@@ -1,19 +1,28 @@
 import { AppRoute } from '@/app';
 import { useAuthStore } from '@/packages/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { Role } from '@/packages/users';
+import { ValueOf } from '@/libs/types';
 
 type PrivateRouteProps = {
   children: React.ReactNode;
+  roles?: ValueOf<typeof Role>[];
 };
 
 const PrivateRoute = (props: PrivateRouteProps) => {
-  const { children } = props;
+  const { children, roles } = props;
 
   const user = useAuthStore((state) => state.user);
+
+  const isAllowedRole = user && roles?.includes(user.role);
 
   const isAuthenticated = Boolean(user);
 
   const location = useLocation();
+
+  if (roles?.length && !isAllowedRole) {
+    return <Navigate to={AppRoute.HOME} state={{ from: location }} replace />;
+  }
 
   return isAuthenticated ? (
     children
