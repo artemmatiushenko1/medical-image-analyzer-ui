a2669a36c7f0f00175a6ec0243838dbfbe5e1e33
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-06-01 10:42:47 +0300

    set dayjs locale

--- src/i18n/helpers.ts
+++ src/i18n/helpers.ts
@@ -3,6 +3,7 @@ import { ValueOf } from '@/libs/types';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { resources } from './resources';
+import dayjs from 'dayjs';
 
 const initi18n = (initialLanguage: ValueOf<typeof Language>) => {
   i18n.use(initReactI18next).init({
@@ -14,4 +15,13 @@ const initi18n = (initialLanguage: ValueOf<typeof Language>) => {
   });
 };
 
-export { initi18n };
+const initDayJs = async (initialLanguage: ValueOf<typeof Language>) => {
+  try {
+    await import(`dayjs/locale/${initialLanguage}`);
+    dayjs.locale(initialLanguage);
+  } catch (error) {
+    console.error(`Error loading locale ${initialLanguage}`, error);
+  }
+};
+
+export { initi18n, initDayJs };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app';
-import '@fontsource/inter';
-import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
@@ -12,9 +10,20 @@ import { useAppStore } from './app';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './libs/packages/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import dayjs from 'dayjs';
+
+import '@fontsource/inter';
+import 'react-toastify/dist/ReactToastify.css';
+
+// TODO: load dynamically. See initDayJs helper
+import 'dayjs/locale/en';
+import 'dayjs/locale/uk';
 
 const renderApp = () => {
-  initi18n(useAppStore.getState().language);
+  const appLanguage = useAppStore.getState().language;
+
+  initi18n(appLanguage);
+  dayjs.locale(appLanguage);
 
   ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
