cd1b2cced51afae31048220585d9c108eb9fe7d9
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-06-19 14:09:30 +0300

    invalidate get all studies query

--- src/pages/new-study/libs/queries/create-study.query.ts
+++ src/pages/new-study/libs/queries/create-study.query.ts
@@ -1,10 +1,19 @@
 import { CreateStudyRequest, studiesApi } from '@/packages/studies';
-import { useMutation } from '@tanstack/react-query';
+import { StudyQueryKey } from '@/pages/studies/libs/enums';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 const useCreateStudy = () => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (request: CreateStudyRequest) => {
       return studiesApi.createStudy(request);
+    },
+    onSuccess: () => {
+      return queryClient.invalidateQueries({
+        queryKey: StudyQueryKey.GET_ALL_STUDIES,
+        refetchType: 'all',
+      });
     },
   });
 };
