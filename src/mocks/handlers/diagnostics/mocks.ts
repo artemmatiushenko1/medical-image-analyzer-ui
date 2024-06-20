1d0c583a218b32c38d59b6e8537763e343517520
artemmatiushenko1 <artom.matyushenko@gmail.com>
2024-06-20 05:32:53 +0300

    fix text

--- src/mocks/handlers/diagnostics/mocks.ts
+++ src/mocks/handlers/diagnostics/mocks.ts
@@ -11,31 +11,35 @@ import {
 const MOCK_DIAGNOSTICS: Diagnostic[] = [
   {
     id: '2',
-    name: 'Детекція пневмонії',
+    name: 'Detection of pneumonia',
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
-    description: 'Some description of diagnostic',
+    description:
+      'Pneumonia is an inflammatory lung condition primarily affecting the alveoli, typically caused by bacterial, viral, or fungal infections. It is characterized by symptoms such as cough, fever, and difficulty breathing, often confirmed by imaging techniques like chest X-rays showing lung consolidation or infiltrates.',
   },
   {
     id: '3',
-    name: 'Діагностування COVID-19',
+    name: 'Diagnosis of COVID-19',
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
-    description: 'Diagnostic description',
+    description:
+      'COVID-19 is a contagious respiratory disease caused by the SARS-CoV-2 virus, leading to symptoms ranging from mild respiratory issues to severe pneumonia. It is characterized by fever, cough, and shortness of breath.',
   },
   {
     id: '4',
-    name: 'Діагностування меланоми',
+    name: 'Diagnosis of melanoma',
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
-    description: 'Diagnostic description',
+    description:
+      'Melanoma is an aggressive form of skin cancer that originates in the melanocytes, the cells responsible for pigment production. It is characterized by the appearance of new or changing moles or skin lesions.',
   },
   {
     id: '5',
-    name: 'Діагностування хвороби Лайма',
+    name: 'Diagnosis of Lyme disease',
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
-    description: 'Diagnostic description',
+    description:
+      'Lyme disease is an infectious disease caused by the bacterium Borrelia burgdorferi, transmitted through the bite of infected black-legged ticks. It is characterized by symptoms such as fever, headache, fatigue, and a distinctive “bull’s-eye” rash.',
   },
 ];
 
@@ -45,7 +49,7 @@ const MOCK_MODEL_VERSIONS: ModelVersion[] = [
     createdAt: dayjs().toISOString(),
     updatedAt: dayjs().toISOString(),
     version: 4,
-    name: 'Increased accuracy',
+    name: 'Increased accuracy up to 99%',
     status: ModelVersionStatus.ENABLED,
   },
   {
@@ -61,7 +65,7 @@ const MOCK_MODEL_VERSIONS: ModelVersion[] = [
     createdAt: dayjs().toISOString(),
     updatedAt: dayjs().toISOString(),
     version: 2,
-    name: 'Increased accuracy',
+    name: 'Enhenced accuracy',
     status: ModelVersionStatus.DISABLED,
   },
   {
@@ -69,7 +73,7 @@ const MOCK_MODEL_VERSIONS: ModelVersion[] = [
     createdAt: dayjs().toISOString(),
     updatedAt: dayjs().toISOString(),
     version: 1,
-    name: 'Increased accuracy',
+    name: 'Initial release',
     status: ModelVersionStatus.DISABLED,
   },
 ];
@@ -93,7 +97,8 @@ const MOCK_MODELS: Model[] = [
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
     status: ModelStatus.ENABLED,
-    description: null,
+    description:
+      'PneumoCheck is an AI model designed for the automated detection of pneumonia from chest X-ray images. It employs deep learning techniques to analyze radiographic features indicative of pneumonia, aiding radiologists in accurate and efficient diagnosis.',
     queueName: crypto.randomUUID(),
     type: MOCK_DIAGNOSTICS[0],
     versions: MOCK_MODEL_VERSIONS,
@@ -105,7 +110,8 @@ const MOCK_MODELS: Model[] = [
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
     status: ModelStatus.ENABLED,
-    description: null,
+    description:
+      'LymeXpert is an AI model developed for the detection and diagnosis of Lyme disease. Using advanced machine learning algorithms, LymeXpert analyzes symptoms, medical history, and potentially relevant diagnostic tests to provide healthcare professionals with a comprehensive assessment for Lyme disease. ',
     queueName: crypto.randomUUID(),
     type: MOCK_DIAGNOSTICS[3],
     versions: MOCK_MODEL_VERSIONS,
@@ -117,7 +123,8 @@ const MOCK_MODELS: Model[] = [
     createdAt: dayjs().format(),
     updatedAt: dayjs().format(),
     status: ModelStatus.ENABLED,
-    description: null,
+    description:
+      'CoviScanNet-2 is an advanced AI model designed for the detection and classification of COVID-19 from chest X-ray and CT scan images. Building upon the original CoviScanNet, this model incorporates state-of-the-art deep learning techniques to enhance accuracy and speed in COVID-19 diagnosis.',
     queueName: crypto.randomUUID(),
     type: MOCK_DIAGNOSTICS[1],
     versions: MOCK_MODEL_VERSIONS,
